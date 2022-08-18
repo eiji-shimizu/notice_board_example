5a0862b517989d73f8f79861b2927828fc009b26
eiji-shimizu <eijishimizu9@gmail.com>
2022-08-18 19:02:14 +0900

    ログアウト機能の追加

--- resources/js/components/HeaderComponent.vue
+++ resources/js/components/HeaderComponent.vue
@@ -1,20 +1,31 @@
 <template>
-  <div>ヘッダー</div>
+  <div>
+    ヘッダー
+    <form action="logout" method="POST">
+      <button type="submit" @click="logout">ログアウト</button>
+      <input type="hidden" name="_token" v-model="state.token" />
+    </form>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, watchEffect } from "vue";
+import { defineComponent, reactive, toRefs, MetaHTMLAttributes } from "vue";
 
 interface State {
-  inputValue: string
-  hasError: boolean
+  token: string;
 }
 
-export default {
-  data() {
-    return {
-      message: "Hello World",
-    };
+export default defineComponent({
+  setup() {
+    let csrfToken: string = "";
+    let meta = document.head.children.namedItem("csrf-token");
+    if (meta !== null) {
+      csrfToken = (meta as MetaHTMLAttributes).content as string;
+    }
+    const state = reactive<State>({
+      token: csrfToken,
+    });
+    return { state };
   },
-};
+});
 </script>
