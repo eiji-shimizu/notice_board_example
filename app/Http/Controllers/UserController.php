59803ec4b2d263ce40c8631bf8081e5144b4c830
eiji-shimizu <eijishimizu9@gmail.com>
2022-08-21 17:25:14 +0900

    ログインユーザー情報取得処理を追加

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,38 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Auth;
+
 class UserController extends Controller
 {
     /**
-     * 新規ユーザー登録
+     * ログインユーザー情報を返す
      *
-     * @return \Illuminate\View\View
+     * @param Request $request
+     * @return JSON response
      */
-    public function register()
+    public function getLoginUserInfo(Request $request)
     {
-        // 1.既に登録済みのユーザー名であった場合 -> 登録不可
-        // 2.登録済みのメールアドレスであった場合 -> 登録不可
-        // 3.それ以外 -> 登録処理
-        // 送られてきたメールアドレスにトークンの値を含んだメール送信
-        // そのメールアドレスにて確認処理がなされた場合ユーザー登録とする
-        return view('welcome');
-    }
-
-    /**
-     * ログイン
-     *
-     * @return \Illuminate\View\View
-     */
-    public function login()
-    {
-        // 1.既に登録済みのユーザー名とパスワードが一致した場合 -> ログイン処理
-        // 2.既に登録済みのメールアドレスとパスワードが一致した場合 -> ログイン処理
-        // メールアドレスにトークンの値を含んだメール送信
-        // そのメールアドレスにて確認処理がなされた場合ログイン可とする
-
-        // 本当は2段階認証の種類を選べるようにして,同じメールアドレスでは確認しない方がよい
-        // また省略のための機能もあった方がよい
-        // しかしいずれも今のところは実装対象外とする
-        return view('welcome');
+        $users =  Auth::user();
+        return response()->json([
+            'name' => $users->name
+        ], 200);
     }
 }
